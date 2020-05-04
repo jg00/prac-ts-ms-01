023318f6659382aaf905e62edeb2ae9fdf27042d
Jack Gamboa <jgamboa929@gmail.com>
2020-05-04 12:03:50 -0500

    Override methods and protected modifier.

---
+++ sec05-classes-and-interfaces/03-overriding-properties-and-protected-modifier/understanding-ts/dist/app.js
@@ -0,0 +1,65 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
+    }
+    Department.prototype.describe = function () {
+        console.log("Department (" + this.id + "): " + this.name);
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log("No. of employees: " + this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("d2", ["Ted"]);
+it.addEmployee("Mel");
+it.addEmployee("Dane");
+it.describe();
+it.printEmployeeInformation();
+var accounting = new AccountingDepartment("d3", []);
+accounting.addReport("Something went wrong...");
+accounting.printReports();
+//# sourceMappingURL=app.js.map
