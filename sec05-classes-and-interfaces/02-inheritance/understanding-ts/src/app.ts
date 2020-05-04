887eb56f0cf619fb7496055038f78831aa4164b0
Jack Gamboa <jgamboa929@gmail.com>
2020-05-04 12:03:07 -0500

    Override methods and protected modifier.

--- sec05-classes-and-interfaces/02-inheritance/understanding-ts/src/app.ts
+++ sec05-classes-and-interfaces/02-inheritance/understanding-ts/src/app.ts
@@ -1,30 +1,14 @@
 class Department {
-  // private readonly id: string;
-  // private name: string;
-  private employees: string[] = [];
+  // Give access to base properties from derived objects.
+  protected employees: string[] = [];
 
-  // Ex 3 Shortcut for auto properties.
-  // Ex 4 Readonly - fields that should not change after initialization
-  constructor(private readonly id: string, public name: string) {
-    // this.id = id;
-    // this.name = n;
-  }
+  constructor(private readonly id: string, public name: string) {}
 
-  // 1 Adding a TS hint:
-  // - By associating 'this' like in describe(this: Department) (ie not really a parameter but more like a hint for TS) we are specifying that when executing the method,
-  // the 'this' should always refer to an instance that is based on the Department class on which the method is executing.
-  // - describe(this: Department) is a special instruction understood by TS.
-  // - Applying the hint also ensures we are adhering to the structure of the class.
-  // So below, 'this: Department' is a hint for TS that specify that 'this' should refer to an instance that is based on Department class.
-  // Improves our code to ensure describe() method is called 'only' on an instance object of type Department.
   describe(this: Department) {
-    // console.log("Department: " + this.name);
     console.log(`Department (${this.id}): ${this.name}`);
   }
 
   addEmployee(employee: string) {
-    // validation code..
-    // this.id = "d2"; // remember id is 'readonly' and cannot change after initialization
     this.employees.push(employee);
   }
 
@@ -34,10 +18,8 @@ class Department {
   }
 }
 
-// 5 Inheritance
-// When you extend, the constructor of the base class is automatically inherited unless you specify a constructor in the derived class that requires a call to the parent constructor using super();
 class ITDepartment extends Department {
-  admins: string[];
+  public admins: string[];
   constructor(id: string, admins: string[]) {
     super(id, "IT");
     this.admins = admins;
@@ -49,6 +31,14 @@ class AccountingDepartment extends Department {
     super(id, "Accounting");
   }
 
+  // Override base method
+  addEmployee(name: string) {
+    if (name === "Max") {
+      return;
+    }
+    this.employees.push(name);
+  }
+
   addReport(text: string) {
     this.reports.push(text);
   }
@@ -58,38 +48,19 @@ class AccountingDepartment extends Department {
   }
 }
 
-// Ex 2 - private and public modifiers
-// const accounting = new Department("d1", "Accounting");
-const it = new ITDepartment("d2", ["Ted"]);
-
+console.log(`-- IT Department --`);
+const it = new ITDepartment("d1", ["Max"]);
 it.addEmployee("Mel");
 it.addEmployee("Dane");
-
-// it.employees[2] = "Anna"; // Issue is this field should be private
-// it.name = "NEW NAME";
-
 it.describe();
 it.printEmployeeInformation();
+console.log(it);
 
-// Ex 1 - 'this' hint on describe(this: Department)
-/* 
-  const accounting = new Department("Accounting");
-  accounting.describe();
-
-  // Note that below is an "object literal" which is a different object from accounting object above.
-  const accountingCopy = {
-    name: "Finance",
-    describe: accounting.describe,
-    // describe: accounting.describe.call({ name: "Sailor3" }), // We could use .call({name: "Sailor3"}) and provide the "name property."
-  }; // passing the accounting.describe function itself
-
-  accountingCopy.describe(); // "Department: undefined" unless we provide the 'this' hint that tells TS that describe() should be called on an object of type Department.
-
-  // We could use .call(), .bind()
-  // accountingCopy.describe.call({ name: "Sailor" });
-  // accountingCopy.describe.bind({ name: "Sailor2" })();
-*/
-
-const accounting = new AccountingDepartment("d3", []);
-accounting.addReport("Something went wrong...");
+console.log("");
+console.log(`-- Accounting Department --`);
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Something went wrong..");
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
 accounting.printReports();
+accounting.printEmployeeInformation();
